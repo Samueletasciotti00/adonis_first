de1f534a6d61e00fd64f98af5b3a3f74c6588ce4
Samuele <samueletasciotti@icloud.com>
2024-10-25 00:47:24 +0200

    feat: User controller funzione di INDEX

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,4 +1,18 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+
+// Import del model User per la CRUD
+import User  from 'App/Models/User';
+
 
 export default class UsersController {
+
+    // Creo una funzione che mi permette di recuperare gli Utenti nel DataBase;
+    public async index({ response }: HttpContext) {
+
+        // Salvo in una variabile tutti gli utenti;
+        const users = await User.all();
+
+        // Ritorno tutti le info degli Users in formato .JSON;
+        return response.json(users);
+    }
 }
